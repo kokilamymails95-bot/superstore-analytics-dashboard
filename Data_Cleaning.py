47bb546568260c69187cca4763add89fc5669776
Kokila <kokilamymails95@gmail.com>
2026-04-09 06:11:54 +0530

    Update Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,9 +1,18 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
 # Remove duplicates
 df = df.drop_duplicates()
 # Handle missing income
 df['Income'] = df['Income'].fillna(df['Income'].median())
 # Convert date column
-df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'], format='%m/%d/%Y').dt.strftime('%d-%m-%Y')
+df['Dt_Customer'] = pd.to_datetime(df['Dt_Customer'], format='%m/%d/%Y')
+#Segment Date & Year
+df['Year'] = df['Dt_Customer'].dt.year
+df['Month'] = df['Dt_Customer'].dt.month
+df['Quarter'] = df['Dt_Customer'].dt.quarter
 # Create Age
 current_year = datetime.now().year
 df['Age'] = current_year - df['Year_Birth']
@@ -15,4 +24,4 @@ with pd.ExcelWriter(output_path, engine='xlsxwriter') as writer:
    df.to_excel(writer,
                sheet_name='Customer_Master',
                index=False)
-   summary.to_excel(writer, sheet_name='summary_Data1', index=False)
+  
